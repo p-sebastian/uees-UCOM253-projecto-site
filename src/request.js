912706735829080648431b908d2a2b49f420d8d4
Sebastian Penafiel <penafiel.sebastian@gmail.com>
2018-10-16 22:24:20 -0500

    changed url

--- src/request.js
+++ src/request.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const URL = 'http://localhost:3002/api';
+const URL = 'http://api.convini.ec:3002/api';
 
 export const insert = async (data) => axios.post(`${URL}/user`, data, { headers: { 'Content-Type': 'application/json' }});
 
